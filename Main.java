9ce2114eab4dc712819fbe9343cf3202888e7a54
Volodymyr Dolidze <dolia1903@gmail.com>
2021-10-04 07:08:49 +0300

    [HW-04] reset 3 commits to make edits

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        //some code
+        System.out.println("Hello, World!");
+        print("This is our 2nd commit for HW-04");
     }
 
+    public static void print(String text) {
+        System.out.println(text);
+    }
 }
